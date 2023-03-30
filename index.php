1600c56d8123ae408df7badea84cc791c6cf2a95
Arturo12321 <valerio.manrique@tecsup.edu.pe>
2023-03-30 11:17:28 -0500

    hola2

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Proyecto web</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-<h1>Desarrollo de Soluciones en la Nube</h1>
+    <h1>Hola soy Arturo</h1>
+    <p>No me gusta el curso, pero que me queda.</p>
 </body>
 </html>
